fe1de51c912234b12fb7fa4c6d19071120ac7423
heppokofrontend <heppokofrontend@gmail.com>
2021-06-18 03:41:53 +0900

    :zap: ガワの追加

---
+++ src/components/Footer.tsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import styles from './Footer.module.scss';
+
+function Footer() {
+  return (
+    <footer className={styles.wrap}>
+      <p>
+        <small>©︎ Coco Kiryu 2020</small>
+      </p>
+    </footer>
+  );
+}
+
+export default Footer;
---
+++ src/components/Header.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import styles from './Header.module.scss';
+
+function Header() {
+  return (
+    <header className={styles.wrap}>
+
+    </header>
+  );
+}
+
+export default Header;
