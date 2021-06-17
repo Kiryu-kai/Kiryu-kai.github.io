ee98ebddc67dd599eb8ba781a28b44875f7da32a
heppokofrontend <heppokofrontend@gmail.com>
2021-06-18 05:16:41 +0900

    :zap: 代紋の読み込み待ち表現

--- src/components/Main.tsx
+++ src/components/Main.tsx
@@ -1,12 +1,17 @@
-import React from 'react';
+import React, {useState} from 'react';
 import styles from './Main.module.scss';
 import BackgroundVideo from './BackgroundVideo';
 
 function Main() {
+  const [loadState, setLoadState] = useState('wait');
+  const logoOnLoad = function () {
+    setLoadState('loaded');
+  };
+
   return (
     <main className={styles.main}>
       <h1 className={styles.h1}>
-        <img className={styles.logo} src="./image/logo.png" alt="桐生会" width={900} height={900} />
+        <img className={styles.logo} src="./image/logo.png" data-state={loadState} onLoad={logoOnLoad} alt="桐生会" width={900} height={900} />
       </h1>
 
       <p className={styles.lede}>
