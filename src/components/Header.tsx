3bacff59e2ea7ae190193e5102a60d5cf2ff749f
heppokofrontend <heppokofrontend@gmail.com>
2021-06-18 05:57:17 +0900

    :sparkles: Add global navigation

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,10 +1,11 @@
 import React from 'react';
+import GlobalNav from './GlobalNav';
 import styles from './Header.module.scss';
 
 function Header() {
   return (
     <header className={styles.wrap}>
-
+      <GlobalNav />
     </header>
   );
 }
