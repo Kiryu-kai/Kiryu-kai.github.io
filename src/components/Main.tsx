71e64e3a3dfaf7b20b611bb924291232c93f3853
heppokofrontend <heppokofrontend@gmail.com>
2021-06-19 03:38:03 +0900

    :zap: 仮の文章を掲載

--- src/components/Main.tsx
+++ src/components/Main.tsx
@@ -1,6 +1,7 @@
 import React, {useState} from 'react';
 import styles from './Main.module.scss';
 import BackgroundVideo from './BackgroundVideo';
+import TextWrap from './TextWrap';
 
 function Main() {
   const [loadState, setLoadState] = useState('wait');
@@ -13,13 +14,18 @@ function Main() {
     <main className={styles.main}>
       <h1 className={styles.h1}>
         <span className={styles.h1__symbol}>
-          <img className={styles.h1__logo} src="./image/logo.png" data-state={loadState} onLoad={logoOnLoad} alt="" width={900} height={900} />
+          <img className={styles.h1__logo} src="./image/logo.png" data-state={loadState} onLoad={logoOnLoad} alt="" width={900} height={900} draggable={false} />
         </span>
 
         <span className={styles.h1__txt}>
           ホロライブ直系 桐生会
         </span>
       </h1>
+
+      <p>指定暴力団ホ口ライブ直参、<TextWrap>伝説のげぼかわドラゴン率いる桐生会。</TextWrap></p>
+      <p>直系組織100団体、<TextWrap>構成員127万人。</TextWrap></p>
+      <p>4代目会長・桐生ココを永遠に語り継ぎ、<TextWrap>推し続けることが我らたつのこの指名。</TextWrap></p>
+      <p>500年の時を経て、<TextWrap>再びその名を世界に轟かせむ。</TextWrap></p>
 
       <BackgroundVideo />
     </main>
