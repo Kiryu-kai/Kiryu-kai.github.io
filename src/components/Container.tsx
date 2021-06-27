a93e21c8614b162fc33762f290feeda2226a3a61
heppokofrontend <heppokofrontend@gmail.com>
2021-06-27 14:18:06 +0900

    :zap: Cursor

--- src/components/Container.tsx
+++ src/components/Container.tsx
@@ -1,12 +1,16 @@
 import React from 'react';
 import { use100vh } from 'react-div-100vh'
 import styles from './Container.module.scss';
+import cursor from '../image/cursor.png';
 
 function Container({ children }: { children: React.ReactNode }) {
   const vh100 = '100vh';
 
   return (
-    <div style={{ minHeight: use100vh() || vh100 }} className={styles.container}>{children}</div>
+    <div style={{
+      minHeight: use100vh() || vh100,
+      cursor: `url(${cursor}), auto`
+    }} className={styles.container}>{children}</div>
   );
 }
 
