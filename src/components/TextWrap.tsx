bf12cd5b482635d4815e0ae8adb13c7762316764
heppokofrontend <heppokofrontend@gmail.com>
2021-07-01 18:48:57 +0900

    :zap: export default を廃止していく予定

--- src/components/TextWrap.tsx
+++ src/components/TextWrap.tsx
@@ -2,7 +2,7 @@ import React from 'react';
 import styles from './TextWrap.module.scss';
 
 /** 改行位置調整のためのspan要素エイリアス */
-function TextWrap({ children }: { children: React.ReactNode }) {
+export function TextWrap({ children }: { children: React.ReactNode }) {
   return (
     <span className={styles.wrap}>{children}</span>
   );
