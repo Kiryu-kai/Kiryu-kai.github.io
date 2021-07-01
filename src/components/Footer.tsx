d8746da0000c2b5d826a9e2f9889bd5ad669a9fc
heppokofrontend <heppokofrontend@gmail.com>
2021-07-01 12:02:16 +0900

    :zap: アイコン配布元サイトへの導線を追加

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -20,12 +20,12 @@ function Footer() {
   return (
     <footer className={styles.wrap}>
       <p className={styles.copyright}>
-        <small>©︎ 2019 Coco Kiryu - <button type="button" className={styles.btn} onClick={onclick}　aria-expanded={isExpanded}>Material</button> - <a href="https://github.com/Kiryu-kai/Kiryu-kai.github.io">Repository</a></small>
+        <small>©︎ 2019 Coco Kiryu - <button type="button" className={styles.btn} onClick={onclick}　aria-expanded={isExpanded}>Materials</button> - <a href="https://github.com/Kiryu-kai/Kiryu-kai.github.io">Repository</a></small>
       </p>
 
       <div className={styles.material} hidden={!isExpanded}>
         <div className={styles.material__inner} onClick={stopPropagetion}>
-          <h2 className={styles.material__hdg}>Material</h2>
+          <h2 className={styles.material__hdg}>Materials</h2>
 
           <p className={styles.material__p}>
             The collaborators are different for each directory.
@@ -44,6 +44,9 @@ function Footer() {
             </li>
             <li className={styles.material__li}>
               <a href="https://commons.nicovideo.jp/material/nc176637">nc176637 - ニコニ・コモンズ</a>
+            </li>
+            <li className={styles.material__li}>
+              <a href="https://icooon-mono.com/12229-%E6%96%B0%E3%81%97%E3%81%84%E3%82%A6%E3%82%A3%E3%83%B3%E3%83%89%E3%82%A6%E3%81%A7%E9%96%8B%E3%81%8F%E3%83%9C%E3%82%BF%E3%83%B3-1/">新しいウィンドウで開くボタン　1 - ICOOON MONO</a>
             </li>
           </ul>
         </div>
